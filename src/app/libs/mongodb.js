27404166cf67ad4ea1da3864889f04d6b134dcdd
Ved257 <ved.purohit25@gmail.com>
2024-07-02 11:14:22 +0530

    test with middleware.js and mongodb.js

--- src/app/libs/mongodb.js
+++ src/app/libs/mongodb.js
@@ -1,30 +1,20 @@
-// lib/mongoose.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+dotenv.config();
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+const connectMongoDB = async () => {
+    if (mongoose.connection.readyState >= 1) return;
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectMongoDB() {
-  if (cached.conn) {
-    return cached.conn;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.log(error);
   }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+};
 
 export default connectMongoDB;
+
+
+
