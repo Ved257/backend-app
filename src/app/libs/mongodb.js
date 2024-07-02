08f454517fc8c1fb5858525e601bfb06ac5b18f2
Ved257 <ved.purohit25@gmail.com>
2024-07-02 11:43:42 +0530

    new mongo connect

--- src/app/libs/mongodb.js
+++ src/app/libs/mongodb.js
@@ -7,13 +7,7 @@ const connectMongoDB = async () => {
     if (mongoose.connection.readyState >= 1) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Example: Increased to 30 seconds
-      socketTimeoutMS: 45000, // Example: Increased to 45 seconds
-    });
-    
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
