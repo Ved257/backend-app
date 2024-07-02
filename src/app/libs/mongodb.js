6ddc442903c4265ed4c99088efceb7daf423c485
Ved257 <ved.purohit25@gmail.com>
2024-07-02 11:38:38 +0530

    test with new mongoConnect

--- src/app/libs/mongodb.js
+++ src/app/libs/mongodb.js
@@ -7,7 +7,13 @@ const connectMongoDB = async () => {
     if (mongoose.connection.readyState >= 1) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Example: Increased to 30 seconds
+      socketTimeoutMS: 45000, // Example: Increased to 45 seconds
+    });
+    
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
